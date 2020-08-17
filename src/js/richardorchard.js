3727cd556c9af8b50d1afa8bc983f845cb53b7b3
Richard Orchard <me@richardorchard.com>
2020-08-17 14:36:39 +0100

    refining javascript code.  still some way to go....

--- src/js/richardorchard.js
+++ src/js/richardorchard.js
@@ -1,89 +1,96 @@
 
-function shuffleArray(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
 
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-
-        // Pick a remaining element...
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-
-        // And swap it with the current element.
-        temporaryValue = array[currentIndex];
-        array[currentIndex] = array[randomIndex];
-        array[randomIndex] = temporaryValue;
-    }
-
-    return array;
-}
-
-
-function loadJSON(callback) {
-
-    var xobj = new XMLHttpRequest();
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'images.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-        if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-        }
-    };
-    xobj.send(null);
-}
+(function () {
 
 
 
+    function shuffleArray(array) {
+        let currentIndex = array.length, temporaryValue, randomIndex;
 
-loadJSON(function (response) {
-    // Parse JSON string into object
-    let imageData = JSON.parse(response);
-    createImageGrid(imageData);
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
 
-});
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
 
-
-
-
-let grid = document.querySelector('.grid');
-
-
-
-function createImageGrid(imageData) {
-
-    let images = shuffleArray(imageData.images);
-    let numberOfImages = images.length;
-
-    for (let index = 0; index < numberOfImages; index++) {
-
-        let div = document.createElement("Div");
-        div.className = 'grid-item';
-
-        let img = document.createElement('img');
-        img.src = images[index].src;
-        img.alt = images[index].description;
-        img.title = images[index].description;
-
-        img.onmouseover = function () {
-            description.innerHTML = (img.title);
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
         }
 
-        div.appendChild(img);
-
-        grid.appendChild(div);
+        return array;
     }
 
-}
+
+    function loadJSON(callback, urlOfJson) {
+
+        let request = new XMLHttpRequest();
+        request.overrideMimeType("application/json");
+        request.open('GET', urlOfJson, true);
+        request.onreadystatechange = function () {
+            if (request.readyState == 4 && request.status == "200") {
+                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+                callback(request.responseText);
+            }
+        };
+        request.send(null);
+    }
 
 
 
-var msnry = new Masonry(grid, {
-    itemSelector: '.grid-item',
-    columnWidth: '.grid-sizer',
-    percentPosition: true
-});
 
-imagesLoaded(grid).on('progress', function () {
-    msnry.layout();
-});
+    loadJSON(function (response) {
+        // Parse JSON string into object
+        let imageData = JSON.parse(response);
+        createImageGrid(imageData);
+
+    }, "images.json");
+
+
+
+    let grid = document.querySelector('.grid');
+
+
+
+    function createImageGrid(imageData) {
+
+        let images = shuffleArray(imageData.images);
+        let numberOfImages = images.length;
+
+
+        for (let index = 0; index < numberOfImages; index++) {
+
+            let div = document.createElement("Div");
+            div.className = 'grid-item';
+
+            let img = document.createElement('img');
+            img.src = images[index].src;
+            img.alt = images[index].description;
+            img.title = images[index].description;
+
+            img.onmouseover = function () {
+                description.innerHTML = (img.title);
+            }
+
+            div.appendChild(img);
+
+            grid.appendChild(div);
+        }
+
+    }
+
+
+
+    let msnry = new Masonry(grid, {
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-sizer',
+        percentPosition: true
+    });
+
+    imagesLoaded(grid).on('progress', function () {
+        msnry.layout();
+    });
+
+}());
